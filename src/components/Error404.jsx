e6fa9481539dd1d18e6b0e04efeb33cff5c791dc
SiddonsD <denise.siddons@gmail.com>
2024-04-05 22:41:13 +0800

    config adobe animate code

--- src/components/Error404.jsx
+++ src/components/Error404.jsx
@@ -6,7 +6,7 @@ const Error404 = () => {
   const domOverlayContainerRef = useRef(null);
 
   useEffect(() => {
-    console.log('useEffect called'); // To check if useEffect is called
+    let comp;
 
     // loads CreateJS library
     const createJsScript = document.createElement('script');
@@ -19,19 +19,20 @@ const Error404 = () => {
 
     // defines initiation code from 404.html and functions within useEffect
     const init = () => {
-      console.log('init function called'); // To check if init is called
+      // Use the refs to get the DOM elements
       const canvas = canvasRef.current;
       const anim_container = animContainerRef.current;
       const dom_overlay_container = domOverlayContainerRef.current;
+
       const comp = AdobeAn.getComposition("AF46AB580DB29048BD31CD62E0AC8625");
       const lib = comp.getLibrary();
       const loader = new createjs.LoadQueue(false);
-      loader.addEventListener("fileload", (evt) => { handleFileLoad(evt, comp) });
+      loader.addEventListener("fileload", handleFileLoad);
       loader.addEventListener("complete", (evt) => { handleComplete(evt, comp) });
       loader.loadManifest(lib.properties.manifest);
     };
 
-    const handleFileLoad = (evt, comp) => {
+    const handleFileLoad = (evt) => {
       const images = comp.getImages();
       if (evt && (evt.item.type === "image")) {
         images[evt.item.id] = evt.result;
@@ -71,43 +72,27 @@ const Error404 = () => {
 
     // load Adobe Animate script after CreateJS has loaded
     createJsScript.onload = () => {
-      console.log('CreateJS script loaded'); // To check if CreateJS script is loaded
-
-      // ensure refs are available in the DOM
-      if (animContainerRef.current && canvasRef.current && domOverlayContainerRef.current) {
-        console.log('animContainerRef is available'); // To check if animContainerRef is available
-
         // Now that CreateJS is loaded, load the Adobe Animate script
         const animateScript = document.createElement('script');
-        animateScript.src = './404.js';
+        animateScript.src = '../404.js';
         animateScript.onerror = () => {
           console.error('Failed to load Adobe Animate script'); // To check if the script failed to load
         };
         animateScript.onload = () => {
-          console.log('Adobe Animate script loaded'); // To check if Adobe Animate script is loaded
-          console.log('AdobeAn:', window.AdobeAn); // Check if AdobeAn is defined
-          // now Adobe Animate script is loaded, initialise animation
-          if (window.AdobeAn && window.init) {
+          // initialise animation
+          if (window.init) {
             window.init();
-          } else {
-            console.error('AdobeAn is not defined.');
           }
         };
       document.body.appendChild(animateScript);
-    } else {
-      console.error('Refs are not available'); // To check if animContainerRef is not available
-    }
   };
 
     // clean up scripts and global functions when component unmounts
     return () => {
       document.body.removeChild(createJsScript);
-      // only remove the animateScript if it was added
-      if (createJsScript.onload) {
-        const animateScript = document.querySelector('script[src="/404.js"]');
+        const animateScript = document.querySelector('script[src="../404.js"]');
         if (animateScript) {
           document.body.removeChild(animateScript);
-        }
       }
       // clean up global functions
       window.init = undefined;

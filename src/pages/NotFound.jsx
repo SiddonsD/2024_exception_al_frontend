db9728823006bfb4345843e848ec56a0cfcff3db
SiddonsD <denise.siddons@gmail.com>
2024-04-05 20:56:43 +0800

    debug static testing

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,13 +1,15 @@
-import Error404 from "../components/Error404";
-import Header from "../components/Header";
-import Footer from "../components/Footer";
+import React from 'react';
+import Error404 from '../components/Error404';
+import Header from '../components/Header';
+import Footer from '../components/Footer';
 
 export default function NotFound() {
   return (
     <div className="not-found-page">
-      <Header />
+      <h1>404 Page Not Found</h1>
+      {/* <Header />
       <Error404 />
-      <Footer />
+      <Footer /> */}
     </div>
   );
 }

19af739fc8eb594f0ebdb0399e5a46a93995149f
SiddonsD <denise.siddons@gmail.com>
2024-04-07 13:16:28 +0800

    page header and footer config

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,17 +1,11 @@
 import Error404 from "../components/Error404";
-import Header from "../components/Header";
-import Footer from "../components/Footer";
 
 export default function NotFound() {
   return (
     <div>
-        <header>
-          <Header />
-        </header>
         <main className="flex-1">
           <Error404 />
         </main>
-        <Footer />
       </div>
   );
 }

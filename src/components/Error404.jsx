d883da95de86b98c800ecd83760717260bd49db1
SiddonsD <denise.siddons@gmail.com>
2024-04-07 17:04:03 +0800

    error 404 adjustment

--- src/components/Error404.jsx
+++ src/components/Error404.jsx
@@ -3,17 +3,17 @@ import Button from './Button';
 
 function Error404() {
   return (
-    <section className="w-full min-h-screen flex flex-col items-center justify-center text-white">
+    <section className="w-full min-h-screen flex flex-col items-center justify-center text-white relative">
       <div className="w-full h-full absolute top-0 left-0 z-0 overflow-hidden">
         <iframe
           src="/404.html"
           title="404 Animated Background"
-          className="w-full h-full object-cover"
+          className="w-full h-full"
           frameBorder="0"
           scrolling="no"
         ></iframe>
       </div>
-      <div className="z-10 py-16">
+      <div className="z-10 py-16 text-center">
         <h1 className="text-5xl lg:text-6xl font-semibold">404</h1>
         <h2 className="text-3xl font-semibold mt-4">Lost your way?</h2>
         <p className="text-2xl mt-4 mb-6">

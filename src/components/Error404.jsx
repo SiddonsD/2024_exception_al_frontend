8e79be24a198762769730cf56c0168ab0d246e02
SiddonsD <denise.siddons@gmail.com>
2024-04-07 14:50:55 +0800

    background image config

--- src/components/Error404.jsx
+++ src/components/Error404.jsx
@@ -3,24 +3,26 @@ import Button from './Button';
 import image404 from '../women-holding-pottery.jpg';
 
 function Error404() {
-  return (
-    <section
-      style={{ backgroundImage: `url(${image404})` }}
-      className="w-full h-screen flex flex-col items-center justify-center bg-no-repeat bg-center bg-cover text-white"
-    >
-      <h1 className="text-5xl lg:text-6xl font-semibold">404</h1>
-      <h2 className="text-3xl font-semibold mt-4">Lost your way?</h2>
-      <p className="text-2xl mt-4 mb-6">
-        The page you are looking for has moved to a galaxy far, far away...
-      </p>
-      <p className="text-2xl mb-6">
-        Let's get you home to safety.
-      </p>
-      <Button buttonType="link" href="/home" size="md" buttonStyle="white">
-        Take me home!
-      </Button>
-    </section>
-  );
-}
-
-export default Error404;
+    return (
+      <section className="w-full h-screen flex flex-col items-center justify-center text-white">
+        <img
+          src="../path/to/your/404-image.jpg"
+          alt="404 Error"
+          className="w-full h-full object-cover absolute top-0 left-0 z-0"
+        />
+        <div className="z-10">
+          <h1 className="text-5xl lg:text-6xl font-semibold">404</h1>
+          <h2 className="text-3xl font-semibold mt-4">Lost your way?</h2>
+          <p className="text-2xl mt-4 mb-6">
+            The page you are looking for has moved to a galaxy far, far away...
+          </p>
+          <p className="text-2xl mb-6">Let's get you home to safety.</p>
+          <Button buttonType="link" href="/home" size="md" buttonStyle="white">
+            Take me home!
+          </Button>
+        </div>
+      </section>
+    );
+  }
+  
+  export default Error404;

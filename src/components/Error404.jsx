d86502a5c6b49a72bdf19e17b47ab4585fd06cd0
SiddonsD <denise.siddons@gmail.com>
2024-04-08 19:02:39 +0800

    update responsive config

--- src/components/Error404.jsx
+++ src/components/Error404.jsx
@@ -3,19 +3,21 @@ import Button from './Button';
 
 function Error404() {
   return (
-    <section className="w-full flex flex-col items-center justify-center text-white relative py-20">
-      <img
-        src="/404-static.png"
-        alt="404 Error page of cartoon space scene with astronaut and spaceship"
-        className="w-full h-full object-cover z-0"
-      />
-      <div className="z-10 text-center">
-        <h1 className="text-5xl lg:text-6xl font-semibold">404</h1>
-        <h2 className="text-3xl font-semibold mt-4">Lost your way?</h2>
-        <p className="text-2xl mt-4 mb-6">
+    <section className="w-full flex items-center justify-center text-white relative">
+      <div className="absolute inset-0">
+        <img
+          src="/404-static.png"
+          alt="404 Error page of cartoon space scene with astronaut and spaceship"
+          className="w-full h-full object-cover object-center"
+        />
+      </div>
+      <div className="z-10 text-center p-8">
+        <h1 className="text-4xl sm:text-5xl md:text-6xl font-semibold">404</h1>
+        <h2 className="text-2xl sm:text-3xl font-semibold mt-4">Lost your way?</h2>
+        <p className="text-lg sm:text-xl mt-4 mb-6">
           The page you are looking for has moved to a galaxy far, far away...
         </p>
-        <p className="text-2xl mb-6">Let's get you home to safety.</p>
+        <p className="text-lg sm:text-xl mb-6">Let's get you home to safety.</p>
         <Button buttonType="link" href="/home" size="md" buttonStyle="white">
           Take me home!
         </Button>

c94e0ebb65d1fd4c037793cf8414f1fb47ec598f
SiddonsD <denise.siddons@gmail.com>
2024-04-08 18:35:50 +0800

    update error 404

--- src/components/Error404.jsx
+++ src/components/Error404.jsx
@@ -2,30 +2,26 @@ import React from 'react';
 import Button from './Button';
 
 function Error404() {
-  return (
-    <section className="w-full min-h-screen flex flex-col items-center justify-center text-white relative">
-      <div className="w-full h-full absolute top-0 left-0 z-0 overflow-hidden">
+    return (
+      <section className="w-full h-screen flex flex-col items-center justify-center text-white">
         <img
-          src="/404-static.png"
-          title="404 Static Background"
-          className="w-full h-full"
-          frameBorder="0"
-          scrolling="no"
-        ></img>
-      </div>
-      <div className="z-10 py-16 text-center">
-        <h1 className="text-5xl lg:text-6xl font-semibold">404</h1>
-        <h2 className="text-3xl font-semibold mt-4">Lost your way?</h2>
-        <p className="text-2xl mt-4 mb-6">
-          The page you are looking for has moved to a galaxy far, far away...
-        </p>
-        <p className="text-2xl mb-6">Let's get you home to safety.</p>
-        <Button buttonType="link" href="/" size="md" buttonStyle="white">
-          Take me home!
-        </Button>
-      </div>
-    </section>
-  );
-}
-
-export default Error404;
+          src="../404static.png"
+          alt="404 Error page of cartoon space scene with astronaut and spaceship"
+          className="w-full h-full object-cover absolute top-0 left-0 z-0"
+        />
+        <div className="z-10">
+          <h1 className="text-5xl lg:text-6xl font-semibold">404</h1>
+          <h2 className="text-3xl font-semibold mt-4">Lost your way?</h2>
+          <p className="text-2xl mt-4 mb-6">
+            The page you are looking for has moved to a galaxy far, far away...
+          </p>
+          <p className="text-2xl mb-6">Let's get you home to safety.</p>
+          <Button buttonType="link" href="/home" size="md" buttonStyle="white">
+            Take me home!
+          </Button>
+        </div>
+      </section>
+    );
+  }
+  
+  export default Error404;
